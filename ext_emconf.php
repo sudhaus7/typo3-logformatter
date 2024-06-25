3e96d4c43ea716a64cc2000c25adee7d6941d077
Frank Berger <fberger@sudhaus7.de>
2024-06-25 22:59:11 +0200

    [TASK] add PHP 8.3 support
    This release adds support for PHP 8.3
    
    Fixing Issue: #2

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF['logformatter'] = [
     'title' => '(Sudhaus7) Logformatter',
     'description' => 'A CLI tool to format and search TYPO3 Logfiles',
     'category' => 'module',
-    'version' => '1.5.2',
+    'version' => '1.5.3',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Frank Berger',
@@ -23,7 +23,7 @@ $EM_CONF['logformatter'] = [
     'author_company' => 'Sudhaus7, ein Label der B-Factor GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.99.99',
+            'typo3' => '10.4.0-12.4.99',
         ],
         'conflicts' => [
         ],
